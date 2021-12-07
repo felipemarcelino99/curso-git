e6cee216b9fbed95f898632b11d4c796fa7bb248
Felipe Marcelino <felipe99marcelino@hotmail.com>
2021-12-07 12:23:25 -0300

    Alterando o programa e utilizando o comando git push para atualizar no GitHub

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -7,7 +7,8 @@ namespace projetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Como você esta hoje? ");
+            Console.WriteLine("Como você esta hoje?");
+            Console.WriteLine("Realizando um push para atualizar o git");
         }
     }
 }
